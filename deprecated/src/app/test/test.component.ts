ef4e1e7ee000a5c383af95e19e4fdc82f589efc5
Yukogan <yuriknor@gmail.com>
2022-11-19 20:39:17 +0100

    deprecated

---
+++ deprecated/src/app/test/test.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'testc',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+[x: string]: any;
+  title = "Test title!";
+  tests = ["1", "2", "3"];
+  
+  // Services
+  
+}
